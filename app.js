6465f7019bab8adb056c9a11a46d8e5a63f79a2e
gabriel.elevado <Gabriel.Elevado@mnfgroup.limited>
2021-05-16 13:58:51 +0800

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Other Worlders!'));
+app.get('/', (req, res) => res.send('Hello ALL Other Worlders!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
